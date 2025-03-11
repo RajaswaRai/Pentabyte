10d574060951bac8f3765c647cde1986ad31574d
RajaswaRai <rajaswarai189@gmail.com>
2025-03-11 13:20:13 +0700

    Remove header

--- resources/js/Pages/Classroom.jsx
+++ resources/js/Pages/Classroom.jsx
@@ -1,13 +1,27 @@
-import { Head } from '@inertiajs/react';
-import Header from "@/Components/Header";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 
+export default function Dashboard({ auth }) {
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Classroom
+                </h2>
+            }
+        >
+            <Head title="Dashboard" />
 
-export default function StudentCardReport() {
-  return (
-    <div>
-      <Head title="Classroom" />
-      <Header />
-      <div className="p-6 text-xl font-bold">Classroom</div>
-    </div>
-  )
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            You're logged in!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
 }
